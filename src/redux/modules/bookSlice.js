cf04e3fe948c1704de348188b979b7256a534e20
sujingkim <suqpus99@gmail.com>
2022-10-17 23:49:42 +0900

    making bookslice reducer

--- src/redux/modules/bookSlice.js
+++ src/redux/modules/bookSlice.js
@@ -1,26 +1,46 @@
-// // src/redux/modules/counterSlice.js
+// src/redux/modules/counterSlice.js
 
-// import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
-// const initialState = {
-//   number: 0,
-// };
+const initialState = {
+  reviews: [
+    {
+      id: 1,
+      title: "영화1 제목",
+      content: "영화1 내용",
+    },
+    {
+      id: 2,
+      title: "영화2 제목",
+      content: "영화2 내용",
+    },
+    {
+      id: 3,
+      title: "영화3 제목",
+      content: "영화3 내용",
+    },
+  ],
+  review: {
+    id: 0,
+    title: "",
+    content: "",
+  },
+};
 
-// const counterSlice = createSlice({
-//   name: 'counter',
-//   initialState,
-//   reducers: {
-//     addNumber: (state, action) => {
-//       state.number = state.number + action.payload;
-//     },
+const bookSlice = createSlice({
+  name: "bookreview",
+  initialState,
+  reducers: {
+    addReview: (state, action) => {
+      return {
+        ...state,
+        reviews: [action.payload, ...state.reviews],
+      };
+    },
+  },
+});
 
-//     minusNumber: (state, action) => {
-//       state.number = state.number - action.payload;
-//     },
-//   },
-// });
-
-// // 액션크리에이터는 컴포넌트에서 사용하기 위해 export 하고
-// export const { addNumber, minusNumber } = counterSlice.actions;
-// // reducer 는 configStore에 등록하기 위해 export default 합니다.
-// export default counterSlice.reducer;
+// 액션크리에이터는 컴포넌트에서 사용하기 위해 export 하고
+export const { addReview } = bookSlice.actions;
+// reducer 는 configStore에 등록하기 위해 export default 합니다.
+export default bookSlice.reducer;
