999970b70f3ef4426ad0a78944f56fc6e930a21d
sujingkim <suqpus99@gmail.com>
2022-10-19 00:29:19 +0900

    thunk function

--- src/redux/modules/bookSlice.js
+++ src/redux/modules/bookSlice.js
@@ -39,6 +39,32 @@ export const __getReviews = createAsyncThunk(
   }
 );
 
+export const __createReviews = createAsyncThunk(
+  "book/createReviews",
+  async (payload, thunkAPI) => {
+    try {
+      const data = await axios.post("http://localhost:3001/reviews", payload);
+      return thunkAPI.fulfillWithValue(data.data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const __deleteReviews = createAsyncThunk(
+  "book/deleteReviews",
+  async (payload, thunkAPI) => {
+    try {
+      const data = await axios.delete(
+        `http://localhost:3001/reviews/${payload}`
+      );
+      return thunkAPI.fulfillWithValue(data.data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
 const bookSlice = createSlice({
   name: "book",
   initialState,
@@ -71,11 +97,17 @@ const bookSlice = createSlice({
     [__getReviews.fulfilled]: (state, action) => {
       state.isLoading = false; // 네트워크 요청이 끝났으니, false로 변경합니다.
       state.reviews = action.payload;
-      console.log(action.payload); // Store에 있는 todos에 서버에서 가져온 todos를 넣습니다.
+      // Store에 있는 todos에 서버에서 가져온 todos를 넣습니다.
     },
     [__getReviews.rejected]: (state, action) => {
       state.isLoading = false; // 에러가 발생했지만, 네트워크 요청이 끝났으니, false로 변경합니다.
       state.error = action.payload; // catch 된 error 객체를 state.error에 넣습니다.
+    },
+    [__createReviews.fulfilled]: (state, { payload }) => {
+      state.isLoading = true; // 네트워크 요청이 끝났으니, false로 변경합니다.
+      state.reviews.push(payload);
+
+      // Store에 있는 todos에 서버에서 가져온 todos를 넣습니다.
     },
   },
 });
